a38b4742fe93dccddcd72ce964168eb6aa62c7f9
Murugesan Karthika <mdkkarthika@gmail.com>
2019-10-14 15:57:31 +0800

    add ArgumentMultimap and ArgumentTokenizer

--- src/main/java/seedu/module/logic/parser/AddDeadlineCommandParser.java
+++ src/main/java/seedu/module/logic/parser/AddDeadlineCommandParser.java
@@ -1,10 +1,12 @@
 package seedu.module.logic.parser;
 
 import static seedu.module.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.module.logic.parser.CliSyntax.PREFIX_DEADLINE;
 
 import java.util.Arrays;
 
 import seedu.module.commons.core.index.Index;
+import seedu.module.commons.exceptions.IllegalValueException;
 import seedu.module.logic.commands.AddCommand;
 import seedu.module.logic.commands.AddDeadlineCommand;
 import seedu.module.logic.parser.exceptions.ParseException;
@@ -18,8 +20,15 @@ public class AddDeadlineCommandParser implements Parser<AddDeadlineCommand>  {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddDeadlineCommand.MESSAGE_USAGE));
         }
-        NameContainsKeywordsPredicate predicate = new NameContainsKeywordsPredicate(Arrays.asList(trimmedArgs));
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_DEADLINE);
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddDeadlineCommand.MESSAGE_USAGE), ive);
+        }
+        String remark = argMultimap.getValue(PREFIX_DEADLINE).orElse("");
 
-        return new AddDeadlineCommand(predicate);
+        return new AddDeadlineCommand(index, remark);
     }
 }
